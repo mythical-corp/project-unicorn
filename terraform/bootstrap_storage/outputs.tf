22b707ee21d74fe68a8d53271afc365fc7b54354
urmuzul <alexandruciocan@protonmail.com>
2025-08-21 19:29:39 +0300

    test

--- terraform/bootstrap_storage/outputs.tf
+++ terraform/bootstrap_storage/outputs.tf
@@ -1,15 +1,15 @@
 output "storage_account_id" {
-    value = azurerm_storage_account.flowlogs.id
+    value = azurerm_storage_account.central_storage.id
 }
 
 output "storage_account_name" {
-    value = azurerm_storage_account.flowlogs.name
+    value = azurerm_storage_account.central_storage.name
 }
 
 output "storage_account_rg" {
-    value = azurerm_resource_group.flowlogs.name
+    value = azurerm_resource_group.central_storage.name
 }
 
 output "storage_account_container" {
-    value = azurerm_storage_container.tfstate.name
+    value = azurerm_storage_container.central_storage.name
 }
