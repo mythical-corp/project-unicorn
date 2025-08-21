ec74f7de0a4352eb7b53fec342927af4d8192d83
urmuzul <alexandruciocan@protonmail.com>
2025-08-21 18:39:37 +0300

    test

--- terraform/bootstrap_storage/main.tf
+++ terraform/bootstrap_storage/main.tf
@@ -24,6 +24,6 @@ resource "azurerm_storage_account" "flowlogs" {
 
 resource "azurerm_storage_container" "tfstate" {
   name = "tfstates"
-  storage_account_id = azurerm_storage_account.flowlogs.id
+  storage_account_name = azurerm_storage_account.flowlogs.name
   container_access_type = "private"
 }
