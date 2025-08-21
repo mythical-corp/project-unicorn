296f6c69bc7fd5157f382a9d33ad768375d3bf8a
urmuzul <alexandruciocan@protonmail.com>
2025-08-21 19:26:49 +0300

    test

--- terraform/bootstrap_storage/main.tf
+++ terraform/bootstrap_storage/main.tf
@@ -1,29 +1,29 @@
-resource "azurerm_resource_group" "flowlogs" {
-  name     = "rg-flowlogs-${var.tenant_name}"
+resource "azurerm_resource_group" "central_storage" {
+  name     = "rg-central-${var.tenant_name}"
   location = var.location
 }
 
-resource "azurerm_storage_account" "flowlogs" {
-    name = "st${replace(lower(var.tenant_name), "-", "")}flowlogs"
-    resource_group_name = azurerm_resource_group.flowlogs.name
-    location = azurerm_resource_group.flowlogs.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_storage_account" "central_storage" {
+  name                     = "stcentral${replace(lower(var.tenant_name), "-", "")}"
+  resource_group_name      = azurerm_resource_group.flowlogs.name
+  location                 = azurerm_resource_group.flowlogs.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-    blob_properties {
-      versioning_enabled = true
-      delete_retention_policy {
-        days = 30
-      }
+  blob_properties {
+    versioning_enabled = true
+    delete_retention_policy {
+      days = 30
     }
+  }
 
-    lifecycle {
-      prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "azurerm_storage_container" "tfstate" {
-  name = "tfstates"
-  storage_account_name = azurerm_storage_account.flowlogs.name
+resource "azurerm_storage_container" "central_storage" {
+  name                  = "tfstates"
+  storage_account_name  = azurerm_storage_account.central_storage.name
   container_access_type = "private"
 }
