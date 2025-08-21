f83bf1d2b43b4d020c3f39cb6ee1d6bc658e24a1
urmuzul <alexandruciocan@protonmail.com>
2025-08-21 22:21:53 +0300

    test

--- terraform/diagnostic_settings/variables.tf
+++ terraform/diagnostic_settings/variables.tf
@@ -28,7 +28,7 @@ variable "policy_category" {
 
 
 locals {
-  policy_json = jsondecode(file("vnet_policy.json"))
+  policy_json = jsondecode(file("vnet-policy.json"))
 
   parameters   = try(local.policy_json.properties.parameters, {})
   policy_rule  = try(local.policy_json.properties.policyRule, {})
